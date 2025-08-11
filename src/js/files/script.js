3aa7bb6f81789b5a2e018a34779dd3fdd2a491d4
VirtoOZ <CenTROY@yandex.ru>
2025-08-11 16:04:35 +0300

    Первый коммит, нужно очистить все лишнее

---
+++ src/js/files/script.js
@@ -0,0 +1,6 @@
+// Подключение функционала "Чертоги Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+
---
+++ src/js/react/components/app/app.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import './app.scss'
+
+export default class App extends Component {
+
+	state = { data: '', }
+
+	render() {
+
+		return (
+			<>
+				<main className="page">
+					<section className="page__main main">
+					</section>
+				</main>
+			</>
+		)
+	}
+}
