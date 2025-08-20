48babe575c6f4989476f2e31fd580103d860123d
VirtoOZ <CenTROY@yandex.ru>
2025-08-20 19:55:33 +0300

    Проходной коммит

--- src/js/react/components/pages/main/main.jsx
+++ src/js/react/components/pages/main/main.jsx
@@ -10,28 +10,28 @@ const Main = () => {
 				<div className="bg__brend"></div>
 				<div className="bg__mountains"></div>
 				<div className="bg__forest"></div>
-			</div>
-			<div className="main__content content">
-				<div className="content__container">
-					<div className="content__subtitle">ПВХ водосток от <span>австрийских профи</span></div>
-					<div className="content__slider slider">
-						<div className="slider__slides">
-							<div className="slider__slide slide">
-								<div className="slide__item item-slide">
-									<div className="item-slide__info info-item-slide">
-										<div className="info-item-slide__kod">Код: 014/114</div>
-										<button type="button" className="info-item-slide__more">Детальнее</button>
+				<div className="main__content content">
+					<div className="content__container">
+						<div className="content__subtitle">ПВХ водосток от <span>австрийских профи</span></div>
+						<div className="content__slider slider">
+							<div className="slider__slides">
+								<div className="slider__slide slide">
+									<div className="slide__item item-slide">
+										<div className="item-slide__info info-item-slide">
+											<div className="info-item-slide__kod">Код: 014/114</div>
+											<button type="button" className="info-item-slide__more">Детальнее</button>
+										</div>
+										<div className="item-slide__image">
+											<img src="@img/main/item-1.png" alt="Защелка-фиксатор" />
+										</div>
+										<div className="item-slide__name">Защёлка фиксатор и дополнительные крепления</div>
 									</div>
-									<div className="item-slide__image-ibg">
-										<img src="@img/main/item-1.png" alt="Защелка-фиксатор" />
-									</div>
-									<div className="item-slide__name">Защёлка фиксатор и дополнительные крепления</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</div></div>
+
 		</main>
 	)
 };
