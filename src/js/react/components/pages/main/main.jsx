30381468a5fb8f0ce5ce2d0e82197f27f0642373
VirtoOZ <CenTROY@yandex.ru>
2025-08-21 19:46:44 +0300

    проработка блока main

--- src/js/react/components/pages/main/main.jsx
+++ src/js/react/components/pages/main/main.jsx
@@ -5,32 +5,29 @@ const Main = () => {
 	return (
 		<main className="page__main main">
 			<div className="main__image-ibg bg">
-				<img src="@img/main/bg.png" alt="Основной фон" />
-				<div className="bg__cloudes"></div>
+				<img className='bg__image' src="@img/main/bg.png" alt="Основной фон" />
+				{/* <div className="bg__cloudes"></div>
 				<div className="bg__brend"></div>
 				<div className="bg__mountains"></div>
-				<div className="bg__forest"></div>
+				<div className="bg__forest"></div> */}
 				<div className="main__content content">
 					<div className="content__container">
 						<div className="content__subtitle">ПВХ водосток от <span>австрийских профи</span></div>
 						<div className="content__slider slider">
-							<div className="slider__slides">
-								<div className="slider__slide slide">
-									<div className="slide__item item-slide">
-										<div className="item-slide__info info-item-slide">
-											<div className="info-item-slide__kod">Код: 014/114</div>
-											<button type="button" className="info-item-slide__more">Детальнее</button>
-										</div>
-										<div className="item-slide__image">
-											<img src="@img/main/item-1.png" alt="Защелка-фиксатор" />
-										</div>
-										<div className="item-slide__name">Защёлка фиксатор и дополнительные крепления</div>
-									</div>
+							<div className="slider__slide slide">
+								<div className="slide__info info-slide">
+									<div className="info-slide__kod">Код: 014/114</div>
+									<button type="button" className="info-slide__more">Детальнее</button>
 								</div>
+								<div className="slide__image-ibg_contain">
+									<img src="@img/main/item-1.png" alt="Защелка-фиксатор" />
+								</div>
+								<div className="slide__name">Защёлка фиксатор и дополнительные крепления</div>
 							</div>
 						</div>
 					</div>
-				</div></div>
+				</div>
+			</div>
 
 		</main>
 	)
